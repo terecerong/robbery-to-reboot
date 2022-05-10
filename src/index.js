429e57828ae3730670e082a89c7f2e19551bea63
adrian <profilte82@gmail.com>
2022-05-10 21:34:33 +0100

    PaintDoor-inpogress

---
+++ src/index.js
@@ -0,0 +1,47 @@
+function RobberyToReboot() {
+    self = this
+    this.doorSprites = document.getElementsByClassName('door')
+    this.doorState = [false, false, false] // false = cerrado & true = open.
+    
+    this.openRandomDoor = function() {
+        const randomDoor = Math.floor(Math.random()* 3)
+        if (!this.doorState[randomDoor]) {
+            this.doorState[randomDoor] = true
+        }
+    }
+
+    /*this.paintDoor = function() {
+        if (this.doorSprites === 'false') {
+            this.doorSprites[randomDoor].classList.remove('doorBrown')
+            this.doorSprites[randomDoor].classList.add('doorBlack')
+        } 
+    }*/
+    //EN PROCESO
+
+    this.start = function() {
+       let timerId = setInterval (function () {
+        self.openRandomDoor()
+        self.paintDoor()
+        console.log(self.doorState)
+       }, 900)
+    }
+}
+
+let robbery = new RobberyToReboot()
+
+robbery.start()
+/*let door = document.getElementsByClassName('door')
+let time = setInterval(function() {
+    
+    if (door[randomDoor].classList.contains('doorBrown')) {
+        door[randomDoor].classList.remove('doorBrown')
+        door[randomDoor].classList.add('doorBlack')
+        } else {
+
+                door[randomDoor].classList.remove('doorBlack')
+                door[randomDoor].classList.add('doorBrown') 
+    }
+    console.log(door[randomDoor])
+    
+}, 500);
+console.log(door[0].classList.contains('doorBrown'))*/
